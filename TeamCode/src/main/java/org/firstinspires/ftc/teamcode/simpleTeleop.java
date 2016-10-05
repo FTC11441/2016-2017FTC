b1de1af699fc4a6d87bbb6760e5012c0b6b0ea19
Ethan Hampton <ethan.hampton@msd.oergonk-12.net>
2016-10-05 15:48:28 -0700

    Modified so it is tank drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleTeleop.java
@@ -17,6 +17,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class simpleTeleop extends OpMode {
     private Robot robot = new Robot();
 
+    double left;
+    double right;
+    double max;
+
     @Override
     public void init() {
         robot.init(hardwareMap);
@@ -25,5 +29,13 @@ public class simpleTeleop extends OpMode {
     @Override
     public void loop() {
 
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        // In this mode the Left stick moves the left wheel forward and backwards and the right moves the right wheel
+        left  = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+
+        //sets the robots motor power for both motors
+        robot.leftMotor.setPower(left);
+        robot.rightMotor.setPower(right);
     }
 }
