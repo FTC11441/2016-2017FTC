0d3fdbae0a69eb8ce41991cfd0ff97a01666dfde
Ethan Hampton <ethan.hampton@msd.oergonk-12.net>
2016-10-17 17:02:00 -0700

    Continued to test Autonomous encoder code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Constants.java
@@ -6,7 +6,7 @@ package org.firstinspires.ftc.teamcode.Utils;
  */
 
 public class Constants {
-    public static final int ENCODER_TICKS_PER_ROTATION = 560;
+    public static final int ENCODER_TICKS_PER_ROTATION = 1560;
     public static final int MOTOR_RPM = 315;
 
 }
