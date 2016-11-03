9f450663ceb7c45fe85149fd73f647a53bf1c43d
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2016-11-02 17:19:37 -0700

    Added beacon and team state info

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configuration/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configuration/Robot.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Configuration;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.Utils.Team;
+
 /**
  * Created by ethan.hampton on 10/25/2016.
  * <p>
@@ -14,14 +16,66 @@ public class Robot extends SimpleRobot {
     public Servo leftBumper = null;
     public Servo rightBumper = null;
 
+    // if 1 then red is on the left and blue is on the right,
+    // if 2 then red is on the right and blue is on the left,
+    // if 0 then we can assume we do not know the state of the variable
+    private byte beaconGears = 0;
+    private byte beaconTools = 0;
+    private byte beaconLegos = 0;
+    private byte beaconWheels = 0;
+
+    /**
+     * the team we are on
+     */
+    private Team team = Team.UNKNOWN;
+
     /* Initialize standard Hardware interfaces */
     public void Init(HardwareMap ahwMap) {
         this.simpleInit(ahwMap);
         leftBumper = ahwMap.servo.get("Left bumper");
         rightBumper = ahwMap.servo.get("Right bumper");
         leftBumper.setDirection(Servo.Direction.REVERSE);
+    }
 
 
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public byte getBeaconGears() {
+        return beaconGears;
+    }
+
+    public void setBeaconGears(byte beaconGears) {
+        this.beaconGears = beaconGears;
+    }
+
+    public byte getBeaconTools() {
+        return beaconTools;
+    }
+
+    public void setBeaconTools(byte beaconTools) {
+        this.beaconTools = beaconTools;
+    }
+
+    public byte getBeaconLegos() {
+        return beaconLegos;
+    }
+
+    public void setBeaconLegos(byte beaconLegos) {
+        this.beaconLegos = beaconLegos;
+    }
+
+    public byte getBeaconWheels() {
+        return beaconWheels;
+    }
+
+    public void setBeaconWheels(byte beaconWheels) {
+        this.beaconWheels = beaconWheels;
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Team.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Utils;
+
+/**
+ * Created by ethan.hampton on 11/2/2016.
+ *
+ * team
+ */
+
+public enum Team {
+    RED, BLUE, UNKNOWN
+}
