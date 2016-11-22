3651ecde77ce57587c3a07fc987aa6427c3fa97b
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2016-11-22 09:36:56 -0800

    added comments and a timeout

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StateAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StateAutoTest.java
@@ -15,6 +15,7 @@ import org.firstinspires.ftc.teamcode.Utils.State;
 @Autonomous(name = "State Auto 1", group = "Test")
 public class StateAutoTest extends OpMode {
     private RobotAuto robot = new RobotAuto();
+    private final double WAIT = 10 * 1000;//wait up to 10 seconds else continue on to next method
 
     @Override
     public void init() {
@@ -40,35 +41,47 @@ public class StateAutoTest extends OpMode {
                 robot.time.reset();
                 robot.currentState = State.MOVE;
                 break;
+
             case MOVE:
                 //if step has default flags than do default action
                 if (robot.steps[robot.currentStep][3] == 0) {
+                    //sets variables to input into drive method of robot
                     double leftMotorRotations = robot.steps[robot.currentStep][0];
                     double rightMotorRotations = robot.steps[robot.currentStep][1];
                     int speed = (int) robot.steps[robot.currentStep][2];
-                    robot.setDrive(leftMotorRotations, rightMotorRotations, speed);
-                    robot.startTime = robot.time.milliseconds();
-                    robot.currentState = State.CHECK;
+
+                    robot.setDrive(leftMotorRotations, rightMotorRotations, speed);//starts robot driving
+                    robot.startTime = robot.time.milliseconds();//sets step start time
+                    robot.currentState = State.CHECK;//changes state to watch for motors to be done
+
                     //if stop flag then move to stop case
                 } else if (robot.steps[robot.currentStep][3] == -1) {
-                    robot.currentState = State.STOP;
+                    robot.currentState = State.STOP;//if flag = -1 then stop program
                 }
                 break;
+
             case WAIT:
                 if (robot.time.milliseconds() >= robot.waitTime) {
                     robot.currentState = State.MOVE;
                 }
                 break;
+
             case CHECK:
-                if (!robot.rightMotor.isBusy() && !robot.leftMotor.isBusy()) {// TODO: 11/16/2016 Implement wait time in order to stop program from catching 
-                    //set proper next step
-                    robot.currentState = State.WAIT;
-                    //set time to wait for next step
-                    robot.waitTime = robot.time.milliseconds() + 1000;
-                    //adds one to current step so that we continue with the program
-                    robot.currentStep++;
+                if (robot.time.milliseconds() < robot.startTime + WAIT) {//insure that robot is no waiting for something that won't happen
+                    if (!robot.rightMotor.isBusy() && !robot.leftMotor.isBusy()) {
+                        //set proper next step
+                        robot.currentState = State.WAIT;
+                        //set time to wait for next step
+                        robot.waitTime = robot.time.milliseconds() + 1000;
+                        //adds one to current step so that we continue with the program
+                        robot.currentStep++;
+                    }
+                } else {//if we think that robot has timed out
+                    robot.currentStep++;//update to current set
+                    robot.currentState = State.MOVE;//we can skip the wait method and go directly to move method
                 }
                 break;
+
             case STOP:
                 //stop and reset robot
                 robot.rightMotor.setPower(0);
