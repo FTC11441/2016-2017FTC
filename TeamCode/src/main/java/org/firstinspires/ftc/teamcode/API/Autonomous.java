3aedaaba03d3dae32bd9e5af9468eb5ea859c0da
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2016-12-01 19:50:04 -0800

    Started to develop Autonomous API to share with other teams

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API/Autonomous.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.API;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by ethan.hampton on 12/1/2016.
+ *
+ * Interface for autonomous classes to extend
+ */
+
+public abstract class Autonomous extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API/ExampleAutonomous.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.API;
+
+/**
+ * Created by ethan.hampton on 12/1/2016.
+ *
+ * An example program to show what we can do with the autonomous API
+ */
+
+public class ExampleAutonomous extends Autonomous {
+
+
+}
